02238aacfb5dcf354e85d91ce2aadd5b67a2e13d
dlav1 <stasguskov1@gmail.com>
2024-12-08 18:35:22 +0300

    Add files via upload

---
+++ Presentation.php
@@ -0,0 +1,12 @@
+<HTML>
+    <HEAD>
+        <meta charset = "utf-8">
+        <title>Курсовой проект</title>
+    </HEAD>
+<BODY>
+<iframe width="1544px" height="720px" src="https://slider-ai.ru/rest/preview?presentation_id=290318#" id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" allow="autoplay; fullscreen"  >
+
+</iframe>
+    
+</BODY>
+</HTML>
